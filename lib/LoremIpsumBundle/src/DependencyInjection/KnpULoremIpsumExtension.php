21fa92d94ad9b2cdf7522b1d89c9cd951b1e3a5b
petrero <petrero@gmail.com>
2018-04-22 10:29:49 +0300

    But services.xml isn't processed automatically. Go to the extension class and remove the var_dump(). The code to load the config file looks a little funny: $ loader = new XmlFileLoader() from the DependencyInjection component. Pass this a new FileLocator - the one from the Config component - with the path to that directory: ../Resources/config. Below that, add $ loader->load('services.xml')
    Voilà! Refresh the page. It works! When the container builds, the load() method is called and our bundle adds its service.
    
    Next, let's talk about service id best-practices, how to support autowiring and public versus private services.

--- lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -3,11 +3,14 @@ namespace KnpU\LoremIpsumBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 class KnpULoremIpsumExtension extends Extension {
   //put your code here
   public function load(array $configs, ContainerBuilder $container) {
-    var_dumo('We\'re alive');die;
+    $loader = new XmlFileLoader($container, new FileLocator(__DIR__. '/../Resources/config'));
+    $loader->load('services.xml');
   }
 
 }
