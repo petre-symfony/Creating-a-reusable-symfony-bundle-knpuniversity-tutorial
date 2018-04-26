06150d93818deba39ff396070b35ec55bd5027c4
petrero <petrero@gmail.com>
2018-04-26 20:15:13 +0300

    Dummy Test Word Provider
    We're going to use the third option: boot a real app with some config, and test the final services. Specifically, I want to test that the custom word_provider config works.
    
    Let's think about this: to create a custom word provider, you need the class, like CustomWordProvider, you need to register it as a service - which is automatic in our app - and then you need to pass the service id to the word_provider option. We're going to do all of that, right here at the bottom of this test class. It's a little nuts, and that's exactly why we're talking about it!
    
    Create a new class called StubWordList and make it implement WordProviderInterface. This will be our fake word provider. Go to the Code -> Generate menu, or Command + N on a Mac, and implement the getWordList() method. Just return an array with two words: stub and stub2.
    Next, copy the testServiceWiring() method, paste it, and rename it to testServiceWiringWithConfiguration(). Remove the last two asserts: we're going to work more on this in a minute.
    Configuring Bundles in the Kernel
    
    Here's the tricky part: we're using the same kernel in two different tests... but we want them to behave differently. In the second test, I need to pass some extra configuration. This will look a bit technical, but just follow me through this.
    
    First, inside the kernel, go back to the Code -> Generate menu, or Command + N on a Mac, and override the constructor. To simplify, instead of passing the environment and debug flag, just hard-code those when we call the parent constructor.
    
    Thanks to that, we can remove those arguments in our two test functions above. But now, add an optional array argument called $ knpUIpsumConfig. This will be the configuration we want to pass under the knpu_lorem_ipsum key.
    
    At the top of the kernel, create a new private variable called $ knpUIpsumConfig, and then assign that in the constructor to the argument.

--- LoremIpsumBundle/tests/FunctionalTest.php
+++ LoremIpsumBundle/tests/FunctionalTest.php
@@ -8,10 +8,11 @@ use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use KnpU\LoremIpsumBundle\KnpULoremIpsumBundle;
 use KnpU\LoremIpsumBundle\KnpUIpsum;
+use KnpU\LoremIpsumBundle\WordProviderInterface;
 
 class FunctionalTest extends TestCase{
   public function testServiceWiring(){
-    $kernel = new KnpULoremIpsumTestingKernel('test', true);
+    $kernel = new KnpULoremIpsumTestingKernel();
     $kernel->boot();
     $container = $kernel->getContainer();
     
@@ -19,10 +20,25 @@ class FunctionalTest extends TestCase{
     $this->assertInstanceOf(KnpUIpsum::class, $ipsum);
     $this->assertInternalType('string', $ipsum->getParagraphs());
   }
+  
+  public function testServiceWiringWithConfiguration(){
+    $kernel = new KnpULoremIpsumTestingKernel();
+    $kernel->boot();
+    $container = $kernel->getContainer();
+    
+    $ipsum = $container->get('knpu_lorem_ipsum-knpu_ipsum');
+    
+  }
 }
 
 class KnpULoremIpsumTestingKernel extends Kernel {
+  private $knpUIpsumConfig;
+public function __construct(array $knpUIpsumConfig = []) {
+  $this->knpUIpsumConfig = $knpUIpsumConfig;
   
+  parent::__construct('test', true);
+}
+
 public function registerBundles(){
   return [
     new KnpULoremIpsumBundle()
@@ -35,4 +51,10 @@ public function registerContainerConfiguration(LoaderInterface $loader) {
 
 }
 
+class stubWordList implements WordProviderInterface{
+  
+  public function getWordList(): array {
+    return ['stub', 'stub2'];
+  }
 
+}
