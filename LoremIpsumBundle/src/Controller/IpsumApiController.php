caaaa65b209a6699c40e9bcfea17e6526259da30
petrero <petrero@gmail.com>
2018-04-27 19:08:34 +0300

    When you create a controller in a reusable bundle, the best practice is to register your controller as a proper service and use dependency injection to get anything you need.
    Add public function __construct() and type-hint the first argument with KnpUIpsum. I'll press Alt+Enter and choose Initialize Fields so that PhpStorm creates and sets a property for that.
    Down below, return $ this->json() - we will not have auto-complete for that method because of the missing AbstractController - with a paragraphs key set to $ this->knpUIpsum->getParagraphs() and a sentences key set to $ this->knpUIpsum->getSentences()
    Excellent!
    
    Registering your Controller as a Service
    
    Next, we need to register this as a service. In services.xml, copy the first service, call this one ipsum_api_controller, and set its class name. For now, don't add public='true' or false: we'll learn more about this in a minute. Pass one argument: the main knpu_lorem_ipsum.knpu_ipsum service.
    Perfect!

--- LoremIpsumBundle/src/Controller/IpsumApiController.php
+++ LoremIpsumBundle/src/Controller/IpsumApiController.php
@@ -2,9 +2,20 @@
 namespace KnpU\LoremIpsumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use KnpU\LoremIpsumBundle\KnpUIpsum;
 
 class IpsumApiController extends AbstractContoller {
+  private $knpUIpsum;
+  
+  public function __construct(KnpUIpsum $knpUIpsum) {
+    $this->knpUIpsum = $knpUIpsum;
+  }
+
+
   public function index(){
-    
+    return $this->json([
+        'paragraphs' => $this->knpUIpsum->getParagraphs(),
+        'sentences' => $this->knpUIpsum->getSentences()
+    ]);
   }  
 }
