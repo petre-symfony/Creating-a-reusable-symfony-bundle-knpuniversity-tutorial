cc2b64cdef7e8f279aefc9423ec23394fdfc54fc
petrero <petrero@gmail.com>
2018-04-27 18:57:41 +0300

    If you watch a lot of KnpU tutorials, you know that I love to talk about how the whole point of a bundle is that it adds services to the container. But, even I have to admit that a bundle can do a lot more than that: it can add routes, controllers, translations, public assets, validation config and a bunch more!
    Find your browser and Google for 'Symfony bundle best practices'. This is a really nice document that talks about how you're supposed to build re-usable bundles. We're following, um, most of the recommendations. It tells you the different directories where you should put different things. Some of these directories are just convention, but some are required. For example, if your bundle provides translations, they need to live in the Resources/translations directory next to the bundle class. If you follow that rule, Symfony will automatically load them.
    
    Adding a Route + Controller
    
    Here's our new goal: I want to add a route & controller to our bundle. We're going to create an optional API endpoint that returns some delightful lorem ipsum text.
    
    Before we start, I'll open my PhpStorm preferences and, just to make this more fun, search for 'Symfony' and enable the Symfony plugin. Also search for 'Composer' and select the composer.json file so that PhpStorm knows about our autoload namespaces.
    
    That's because the class lives in FrameworkBundle and... remember! Our bundle does not require that! Ignore this problem for now. Instead, find our app code, open AbstractController, copy its namespace, and use it to add the use statement manually to the controller.
    Next, add a public function called index. Here, we're going to use the KnpUIpsum class to return a JSON response with some dummy text.

---
+++ LoremIpsumBundle/src/Controller/IpsumApiController.php
@@ -0,0 +1,10 @@
+<?php
+namespace KnpU\LoremIpsumBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IpsumApiController extends AbstractContoller {
+  public function index(){
+    
+  }  
+}
