16d3c7f169e3c9e248aaa2486fd6b62abdf5aecc
petrero <petrero@gmail.com>
2018-04-29 10:22:29 +0300

    The Special @Event Documentation
    But the coolest part is this: add @Event(), and then inside double quotes, put the full class name of the event that listeners will receive. In other words, copy the namespace from the event class, paste it here and add \FilterApiResponseEvent.
    
    What the heck does this do? On a technical level, absolutely nothing! This is purely documentation. But! Some systems - like PhpStorm - know to parse this and use it to help us when we're building event subscribers. We'll see exactly what I'm talking about in a minute. But, it's at least good documentation: if you listen to this event, this is the event object you should expect.

--- LoremIpsumBundle/src/Event/KnpULoremIpsumEvents.php
+++ LoremIpsumBundle/src/Event/KnpULoremIpsumEvents.php
@@ -7,6 +7,8 @@ final class KnpULoremIpsumEvents {
    * Called directly before the Lorem Ipsum API is returned
    * 
    * Listeners have the opportunity to change that data
+   * 
+   * @Event("KnpU\LoremIpsumBundle\Event\FilterApiResponseEvent")
    */
   const FILTER_API = 'knpu_forem_ipsum.filter_api';
 }
